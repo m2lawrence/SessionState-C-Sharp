b38cfce5eb016f82db6668da8dcd14e21a7d61e8
Michael Lawrence BSc (HONS) <m2lawrence77@outlook.com>
2017-12-30 17:45:47 +0000

    Add files via upload

---
+++ SessionState2.aspx
@@ -0,0 +1,15 @@
+﻿<%@ Page Title="SessionState2" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="SessionState2.aspx.cs" Inherits="SessionState.SessionState2" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div class="jumbotron">
+        <h1>SessionState2</h1>
+    </div>
+
+    <div class="row">
+        <div class="col-md-4">
+            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox><asp:Button ID="Button1" runat="server" Text="Button" OnClick="Button1_Click" />
+        </div>       
+    </div>
+
+</asp:Content>
---
+++ SessionState2.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SessionState
+{
+    public partial class SessionState2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                if (Session["Clicks"] == null)
+                {
+                    Session["Clicks"] = 0;
+                }
+                TextBox1.Text = Session["Clicks"].ToString();
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            //here the int variable ClicksCount, stores the Session clicks and increments up 1.
+            int ClicksCount = (int)Session["Clicks"] + 1;
+            //here convert int to string and passes the Session memory in the TextBox1.
+            TextBox1.Text = ClicksCount.ToString();
+            //here the Session memory gets stored back into the int variable ClicksCount.
+            Session["Clicks"] = ClicksCount;
+        }
+    }
+}
